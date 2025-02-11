b531fc16fb39a3bf0e283d7d3d32d4c6099baaf4
AnastasyiaMaximova <rsk.maximova@bk.ru>
2025-02-11 19:18:27 +0300

    Initial Commit

---
+++ EiffelTowerSwiftUI/EiffelTowerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EiffelTowerSwiftUIApp.swift
+//  EiffelTowerSwiftUI
+//
+//  Created by Anastasya Maximova on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct EiffelTowerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
